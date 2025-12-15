164326f0d18267fbd51d8f4536503a1df7986a7c
DESKTOP-MHCDLIE\school02-10 <a.hori.topaz@gmail.com>
2025-12-15 10:37:49 +0900

    Revert "堀　ファイル名をOriginalに改名したjavaファイル"
    This reverts commit 45dfc16574d4fa1038e6eabb92f7ebec2e8076fd.

--- src/main/java/jp/co/kikin/service/EmployeeMstMntLogic.java
+++ src/main/java/jp/co/kikin/service/EmployeeMstMntLogic.java
@@ -24,7 +24,7 @@ import jp.co.kikin.constant.DbConstant.M_employee;
  * @author naraki
  *
  */
-public class EmployeeMstMntLogicOriginal {
+public class EmployeeMstMntLogic {
 
     /**
      * 社員マスタの更新系の処理を行う
